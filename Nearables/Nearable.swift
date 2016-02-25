9614f43af1725fda779b5456c88d2738d930fdbe
pavoni <mail@gregdowling.com>
2016-02-25 14:28:41 +0000

    first commit

---
+++ Nearables/Nearable.swift
@@ -0,0 +1,9 @@
+//
+//  Nearable.swift
+//  Nearables
+//
+//  Created by Greg Dowling on 23/02/2016.
+//  Copyright © 2016 Greg Dowling. All rights reserved.
+//
+
+import Foundation
---
+++ Nearables/ObjCBridge.h
@@ -0,0 +1,13 @@
+//
+//  ObjCBridge.h
+//  Nearables
+//
+//  Created by Greg Dowling on 23/02/2016.
+//  Copyright © 2016 Greg Dowling. All rights reserved.
+//
+
+#ifndef ObjCBridge_h
+#define ObjCBridge_h
+
+
+#endif /* ObjCBridge_h */
